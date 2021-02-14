4b7e2e58509fefe2303342a4abd2f31e78c8875b
Jesus Alvarez <alv.mtz94@gmail.com>
2021-02-14 11:21:18 +0100

    updated config and setup files

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Jesus Alvarez',                   # Type in your name
   author_email = 'alv.mtz94@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/jsgilberto/dyna_orm',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/jsgilberto/dyna_orm/archive/v0.0.1.tar.gz',    # I explain this later on
   keywords = ['Dynamodb', 'AWS', 'ORM'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'boto3==1.16.32'
