7beb4c9992ef1112d67d5e2e6267cff2d15ed254
Jesus Alvarez <alv.mtz94@gmail.com>
2021-02-14 20:06:02 +0100

    fixed import statements

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from db.table import Table
-from utils.exceptions import IncorrectDataError, InstanceNotFound
+from dyna_orm.db.table import Table
+from dyna_orm.utils.exceptions import IncorrectDataError, InstanceNotFound
